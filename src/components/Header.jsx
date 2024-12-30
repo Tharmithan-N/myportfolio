1a82d9a616ce5029b991678f635da2f24dafd57e
Tharmithan <tharmithan@orelit.com>
2024-12-30 08:19:04 +0530

    name change

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ import ThemeToggle from "./ThemeToggle";
 const Header = () => {
   return (
     <div className="flex justify-between py-8 sm:py-12">
-      <h1 className="text-3xl text-primary font-semibold">AS</h1>
+      <h1 className="text-3xl text-primary font-semibold">NT</h1>
       <ul className="gap-5 hidden text-xl sm:flex">
         <a
           href="#projects"
